36f0a8e6dd03ef266b0a1076509faa642c50f60f
nicole-nxn <nicolenxn1234@gmail.com>
2026-06-22 18:33:26 +1000

    add time

--- src/app/shared/components/task-card/task-card.ts
+++ src/app/shared/components/task-card/task-card.ts
@@ -1,11 +1,12 @@
 import { Component, input, output } from '@angular/core';
+import { DecimalPipe, TitleCasePipe } from '@angular/common';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { Task } from '../../models/task.model';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-task-card',
-  imports: [MatCheckboxModule, MatIconModule],
+  imports: [MatCheckboxModule, MatIconModule, TitleCasePipe, DecimalPipe],
   templateUrl: './task-card.html',
 })
 export class TaskCard {
--- src/app/shared/components/time-selector/time-selector.ts
+++ src/app/shared/components/time-selector/time-selector.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, input, linkedSignal, output } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTimepickerModule } from '@angular/material/timepicker';
+import { Time } from '../../models/time.model';
+import { Season } from '../../models/season.model';
 
 @Component({
   selector: 'app-time-selector',
@@ -9,5 +11,48 @@ import { MatTimepickerModule } from '@angular/material/timepicker';
   templateUrl: './time-selector.html',
 })
 export class TimeSelector {
+  initialTime = input<Time>();
+  timeChanged = output<Time>();
+
+  protected readonly Season = Season;
   days = Array.from({ length: 28 }, (_, i) => i + 1);
+
+  selectedYear = linkedSignal(() => this.initialTime()?.year ?? 1);
+  selectedSeason = linkedSignal(() => this.initialTime()?.season ?? Season.SPRING);
+  selectedDay = linkedSignal(() => this.initialTime()?.day ?? 1);
+  selectedHourMinute = linkedSignal(() => {
+    const time = this.initialTime();
+    return time ? new Date(0, 0, 0, time.hour, time.minute) : null;
+  });
+
+  private emit() {
+    const d = this.selectedHourMinute();
+    this.timeChanged.emit({
+      year: this.selectedYear(),
+      season: this.selectedSeason(),
+      day: this.selectedDay(),
+      hour: d?.getHours() ?? 0,
+      minute: d?.getMinutes() ?? 0,
+    });
+  }
+
+  onYearChange(event: Event) {
+    this.selectedYear.set(Number((event.target as HTMLInputElement).value));
+    this.emit();
+  }
+
+  selectSeason(season: Season) {
+    this.selectedSeason.set(season);
+    this.emit();
+  }
+
+  selectDay(day: number) {
+    this.selectedDay.set(day);
+    this.emit();
+  }
+
+  onTimeChange(date: Date | null) {
+    this.selectedHourMinute.set(date);
+    this.emit();
+  }
 }
